459ce32de6551689cc0eb1df3ee1c2dd0d650c65
TBD <weinanshao@163.com>
2018-04-02 17:32:35 +0800

    Initial Commit

---
+++ TPreventKVC/TPreventKVC.h
@@ -0,0 +1,19 @@
+//
+//  TPreventKVC.h
+//  TPreventKVC
+//
+//  Created by TBD on 2018/4/2.
+//  Copyright © 2018年 TBD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPreventKVC.
+FOUNDATION_EXPORT double TPreventKVCVersionNumber;
+
+//! Project version string for TPreventKVC.
+FOUNDATION_EXPORT const unsigned char TPreventKVCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPreventKVC/PublicHeader.h>
+
+
