7bfa0f3c0a9d90f750d17e5693052e97da0fbddd
TBD <weinanshao@163.com>
2018-04-19 11:12:42 +0800

    修改添加Safe Exchange Method

--- TPreventKVC/NSObject+PreventKVC.m
+++ TPreventKVC/NSObject+PreventKVC.m
@@ -9,6 +9,56 @@
 #import "NSObject+PreventKVC.h"
 #import <objc/runtime.h>
 
+
+#pragma mark - Safe Exchange Method
+#pragma mark -
+
+static inline
+void __tp_kvc_exchange_instance_method(Class cls, SEL originalSel, SEL swizzledSel) {
+    Method originalMethod = class_getInstanceMethod(cls, originalSel);
+    Method swizzledMethod = class_getInstanceMethod(cls, swizzledSel);
+    
+    // 交换实现进行添加函数
+    BOOL addOriginSELSuccess = class_addMethod(cls,
+                                               originalSel,
+                                               method_getImplementation(swizzledMethod),
+                                               method_getTypeEncoding(swizzledMethod));
+    BOOL addSwizzlSELSuccess = class_addMethod(cls,
+                                               swizzledSel,
+                                               method_getImplementation(originalMethod),
+                                               method_getTypeEncoding(originalMethod));
+    // 全都添加成功，返回
+    if (addOriginSELSuccess && addSwizzlSELSuccess) {
+        return;
+    }
+    // 全都添加失败，已经添加过了方法，交换
+    if (!addOriginSELSuccess && !addSwizzlSELSuccess) {
+        method_exchangeImplementations(originalMethod,
+                                       swizzledMethod);
+        return;
+    }
+    // addOriginSELSuccess 成功，addSwizzlSELSuccess 失败，replace SwizzlSel
+    if (addOriginSELSuccess && !addSwizzlSELSuccess) {
+        class_replaceMethod(cls,
+                            swizzledSel,
+                            method_getImplementation(originalMethod),
+                            method_getTypeEncoding(originalMethod));
+        return;
+    }
+    // addSwizzlSELSuccess 成功，addOriginSELSuccess 失败，replace originSEL
+    if (!addOriginSELSuccess && addSwizzlSELSuccess) {
+        class_replaceMethod(cls,
+                            originalSel,
+                            method_getImplementation(swizzledMethod),
+                            method_getTypeEncoding(swizzledMethod));
+        return;
+    }
+}
+
+
+#pragma mark - Implementation Replace
+#pragma mark -
+
 @implementation NSObject (PreventKVC)
 
 #pragma mark - HandleKVCErrorBlock
@@ -25,12 +75,15 @@
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        method_exchangeImplementations(class_getInstanceMethod(self, @selector(setValue:forUndefinedKey:)),
-                                       class_getInstanceMethod(self, @selector(__t_setValue:forUndefinedKey:)));
-        method_exchangeImplementations(class_getInstanceMethod(self, @selector(valueForUndefinedKey:)),
-                                       class_getInstanceMethod(self, @selector(__t_valueForUndefinedKey:)));
-        method_exchangeImplementations(class_getInstanceMethod(self, @selector(setNilValueForKey:)),
-                                       class_getInstanceMethod(self, @selector(__t_setNilValueForKey:)));
+        __tp_kvc_exchange_instance_method(self,
+                                          @selector(setValue:forUndefinedKey:),
+                                          @selector(__t_setValue:forUndefinedKey:));
+        __tp_kvc_exchange_instance_method(self,
+                                          @selector(valueForUndefinedKey:),
+                                          @selector(__t_valueForUndefinedKey:));
+        __tp_kvc_exchange_instance_method(self,
+                                          @selector(setNilValueForKey:),
+                                          @selector(__t_setNilValueForKey:));
     });
 }
 
